ef08a1ba0c0bf455964594592ad90044e5448249
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:56:54 +0200

    Sconti concorrenza

---
+++ ScontiConcorrenza/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ScontiConcorrenza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double importo = 0;
+            double sconto = 0;
+            double importo_scontato = 0;
+            Console.WriteLine("Inserisci l'importo: ");
+            importo = double.Parse(Console.ReadLine());
+            if (importo <= 300)
+            {
+                sconto = (importo / 100) * 10;
+                importo_scontato = importo - sconto;
+                Console.WriteLine("Lo sconto è di: " + sconto);
+                Console.WriteLine("L'importo è di: " + importo_scontato);
+            }
+            else if (importo > 300)
+            {
+                sconto = (importo / 100) * 20;
+                importo_scontato = importo - sconto;
+                Console.WriteLine("Lo sconto è di: " + sconto);
+                Console.WriteLine("L'importo è di: " + importo_scontato);
+            }
+            Console.ReadLine();
+        }
+    }
+}
